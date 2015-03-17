0590bef1aecf691bd64cf393b9fd85a897723e6c
Bala Kumaran <bkumarans@gmail.com>
2015-03-17 22:37:53 +0530

    Update CoreResolver.py

--- CoreResolver.py
+++ CoreResolver.py
@@ -4,20 +4,20 @@ import subprocess
 import sys
 import uuid
 
-env1 = os.system("set sysroot i/home/team_auto/XidToolChains/XiD_BCM_URSR/toolchains/stbgcc-4.5.4-2.7/arm-linux-uclibcgnueabi/sys-root/")
-env2 = os.system("set solib-search-path /home/team_auto/XidToolChains/XiD_BCM_URSR/toolchains/stbgcc-4.5.4-2.7/arm-linux-uclibcgnueabi/sys-root/")
+env1 = os.system("set sysroot i/home/team_auto/toolchains/gcc-4.5.4-2.7/arm-linux-uclibcgnueabi/sys-root/")
+env2 = os.system("set solib-search-path /home/team_auto/toolchains/gcc-4.5.4-2.7/arm-linux-uclibcgnueabi/sys-root/")
 
 coredump = str(sys.argv[1])
 
 newfilename = "Coredump"+str(uuid.uuid4())
 
 if os.path.exists(coredump):
-    os.system("mv "+coredump+" /home/team_auto/pyexec/tests/pkdta/CoreDumpResolve/coredumps/"+newfilename)
+    os.system("mv "+coredump+" /home/team_auto/tests/CoreDumpResolve/coredumps/"+newfilename)
     if (os.path.exists(coredump) & env1==0 & env2==0):
-        p = subprocess.Popen(['/home/team_auto/XidToolChains/XiD_BCM_URSR/toolchains/stbgcc-4.5.4-2.7/bin/arm-linux-gdb'],stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen(['/home/team_auto/toolchains/gcc-4.5.4-2.7/bin/arm-linux-gdb'],stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         time.sleep(1)
          
-        p.stdin.write('core '+'/home/team_auto/pyexec/tests/pkdta/CoreDumpResolve/coredumps/'+newfilename+'\n')
+        p.stdin.write('core '+'/home/team_auto/tests/CoreDumpResolve/coredumps/'+newfilename+'\n')
 
         time.sleep(0.5)
 
